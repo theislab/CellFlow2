f591e18bef89f93bfc00bdb16966efd9d3d33f5d
Xiaotong Fu <xiaotong.fu@helmholtz-munich.de>
2026-06-24 13:02:35 +0200

    recon

--- src/scaleflow/model/__init__.py
+++ src/scaleflow/model/__init__.py
@@ -1,3 +1,17 @@
+from scaleflow.model._recon import (
+    Autoencoder,
+    Decoder,
+    Encoder,
+    ReconDecoder,
+    reconstruction_loss,
+)
 from scaleflow.model._scaleflow import ScaleFlow
 
-__all__ = ["ScaleFlow"]
+__all__ = [
+    "ScaleFlow",
+    "Encoder",
+    "Decoder",
+    "Autoencoder",
+    "ReconDecoder",
+    "reconstruction_loss",
+]
